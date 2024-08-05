23a64a6c535d284d4152afa81dffc015c01bd72d
stasyyellow <anastasia.nazinyan9@gmail.com>
2024-08-05 17:26:05 +0400

    files

--- main3.py
+++ main3.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Настройка Selenium WebDriver
@@ -17,29 +19,36 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Функция для парсинга цен на диваны с сайта divan.ru
 def parse_prices(url):
-    driver.get(url)
+    driver.get(url)  # Переход на заданный URL (divan.ru)
     time.sleep(5)  # Ждем загрузки страницы
 
     prices = []
     while True:
-        items = driver.find_elements(By.CLASS_NAME, 'product-price__current')
+        items = driver.find_elements(By.CLASS_NAME, 'ui-LD-ZU.KIkOH')  # Получаем элементы с классом 'product-price__current'
         for item in items:
-            price_text = item.text
-            price = int(price_text.replace('₽', '').replace(' ', ''))
-            prices.append(price)
+            price_text = item.text  # Извлекаем текст цены
+            price_text = price_text.replace('руб', '').replace(' ', '').replace('.', '')  # Преобразуем цену в числовой формат
+            try:
+                price = int(price_text)
+                prices.append(price)
+            except ValueError:
+                print(f"Невозможно преобразовать в int: {price_text}")
 
         try:
-            next_button = driver.find_element(By.CSS_SELECTOR, 'a.pagination-next')
-            next_button.click()
+            # Используем явные ожидания для нахождения кнопки "Далее"
+            next_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.wDTtf.cursor-default-hover'))
+            )
+            next_button.click()  # Кликаем по кнопке
             time.sleep(5)  # Ждем загрузки следующей страницы
         except Exception as e:
-            print(f"Ошибка: {e}")
-            break
+            print(f"Ошибка: {e}")  # Выводим ошибку, если переход на следующую страницу не удался
+            break  # Прекращаем цикл, если нет кнопки "Далее"
 
     return prices
 
 # URL сайта divan.ru
-url = 'https://www.divan.ru/category/divany'
+url = 'https://www.divan.ru/category/divany-i-kresla'
 
 # Парсинг цен на диваны
 prices = parse_prices(url)
@@ -65,3 +74,5 @@ plt.xlabel('Цена (₽)')
 plt.ylabel('Частота')
 plt.grid(True)
 plt.show()
+   
+#ui-jDl24 cursor-default-hover
